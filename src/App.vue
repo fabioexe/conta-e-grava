b209718743f64b6c0dab2e031ff29606f6caf2a7
Fabio Antunes <fabio.avasc@gmail.com>
2021-07-12 18:56:22 -0300

    Inicio da interface grafica e logica do contador

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,87 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+    <v-card height="700" width="700">
+      <div class="buttons">
+        <div class="plus-button">
+          <v-btn class="mx-2" fab dark large color="purple" @click="counterplus">
+            <p> {{ counter }} </p>
+          </v-btn>
+        </div>
+        <div class="other-options">
+          <v-btn class="mx-2" fab dark color="indigo">
+            <v-icon dark> mdi-content-save-all </v-icon>
+          </v-btn>
+          <v-btn class="mx-2" fab dark color="indigo" @click="counterminus">
+            <v-icon dark> mdi-minus </v-icon>
+          </v-btn>
+        </div>
       </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+//      <OverlayButtonForCalendar />
+    </v-card>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+
+//import OverlayButtonForCalendar from "./components/OverlayButtonForCalendar.vue"
+//import { mdiContentSaveAll } from '@mdi/js'
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+  //  OverlayButtonForCalendar
   },
 
   data: () => ({
-    //
+    counter: 0
   }),
+  methods: {
+    counterplus(){
+      console.log(this.counter)
+      this.counter += 1
+    },
+    counterminus(){
+      if (this.counter > 0){
+        console.log(this.counter)
+        this.counter -= 1
+      } else {
+        this.counter = 0
+      }
+    }
+  },
 };
 </script>
+
+<style scoped>
+.template .v-app {
+  display: flex;
+  justify-content: center;
+}
+
+.buttons {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 50px;
+  margin-top: 50px;
+}
+
+.other-options {
+  display: flex;
+  flex-direction: column;
+}
+
+.other-options button {
+  margin: 10px 0 ;
+}
+
+.buttons .plus-button .mx-2 {
+  display: flex;
+  width: 300px;
+  height: 300px;
+}
+
+.mx-2 p {
+  font-size: 50px;
+}
+</style>
---
+++ src/components/OverlayButtonForCalendar.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-row justify="center">
+    <v-btn class="white--text btn-abrir" color="teal" @click="overlay = !overlay">
+      Abrir Calendário
+    </v-btn>
+
+    <v-overlay :value="overlay">
+        <Calendar class="calendar"/>
+      <v-btn class="white--text btn-fechar" color="teal" @click="overlay = false">
+        Fechar Calendário
+      </v-btn>
+    </v-overlay>
+  </v-row>
+</template>
+
+<script>
+
+import Calendar from "../components/Calendar.vue"
+
+  export default {
+    
+    components: { Calendar },  
+
+    data: () => ({
+      overlay: false,
+    }),
+  }
+</script>
+
+<style>
+.v-overlay{
+    display: flex;
+}
+
+.calendar {
+    width: 100%;
+    height: 100%;
+}
+
+.v-calendar-weekly__day {
+    width: 100px;
+}
+
+</style>
